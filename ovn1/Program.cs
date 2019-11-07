f2a2be87a6ae53f7a505d3fdafe4f8c2d1a9fde5
Karin Skans <skka19st@student.ju.se>
2019-11-07 13:10:47 +0100

    ovning 1

---
+++ ovn1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ovn1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // skriver till skärmen
+            Console.WriteLine("Vad heter du?");
+
+            // läser från skärmen, till variabeln namn
+            var namn = Console.ReadLine();
+
+            // skriver välkomsthälsning
+            Console.WriteLine("Välkommen hit, " +  namn + "!");
+        }
+    }
+}
